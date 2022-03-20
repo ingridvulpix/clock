01af1a2bb6efec142560f17bedb2a49a198b3609
ingridvulpix <ingridalmeidafs@gmail.com>
2022-03-20 19:57:17 -0300

    Current time

--- apple_clock.py
+++ apple_clock.py
@@ -1,4 +1,15 @@
-from datetime import datetime , timedelta
+from datetime import *
+from tkinter import *
 from math import sin , cos , pi
-class Clock:
-    pass
+
+class Clock():
+    def __init__(self, tz_home):
+        self.tz_home = tz_home
+
+    def get_time(self): #data da UTC -3
+        current_time = datetime.now()
+        print ("initial_date:\n", str(current_time))
+
+
+teste1 = Clock(-3,5)
+teste1.get_time()
