3ebf434fa69b2c3fe3d9cf6e3b1bb71a75fae90f
ingridvulpix <ingridalmeidafs@gmail.com>
2022-03-21 00:03:14 -0300

    Add red hand

--- apple_clock.py
+++ apple_clock.py
@@ -1,9 +1,14 @@
-from datetime import *
+from datetime import datetime
 import math
 import tkinter as Tkinter
 from math import *
 import time
+import pytz
 import json
+
+#TO DO: Make clock image with relative size
+#TO DO: Documentation
+#TO DO: Fix for form red hand, so it moves
 
 class Clock(Tkinter.Tk):
     def __init__(self):
@@ -18,6 +23,7 @@ class Clock(Tkinter.Tk):
         self.canvas_shapes()
         self.creating_bg()
         self.creating_hands()
+        self.extra_hand()
         return
     
     # Back-ground
@@ -36,10 +42,17 @@ class Clock(Tkinter.Tk):
     # Creating Moving hands
     def creating_hands(self):
         self.hands=[]
+        self.hand = []
         for i in range(3):
             store=self.canvas.create_line(self.x, self.y,self.x+self.length,self.y+self.length,width=2, fill='black')
             self.hands.append(store)
+        store=self.canvas.create_line(self.x, self.y,self.x+self.length,self.y+self.length,width=2, fill='red')
+        self.hand.append(store)        
         return
+
+    def extra_hand(self):
+        pass
+
 
     # Function for updates
     def update_class(self):
@@ -47,6 +60,11 @@ class Clock(Tkinter.Tk):
         t = time.strptime(str(now.tm_hour), "%H")
         hour = int(time.strftime( "%I", t ))*5
         now=(hour,now.tm_min,now.tm_sec)
+        #Home timezone
+        home = datetime.now(pytz.timezone('Asia/Kolkata'))
+        time_home = time.strptime(str(home.hour),"%H")
+        hour_home = int(time.strftime( "%I", time_home ))*5
+        clock_home = (hour_home,home.hour,home.second)
         # Changing hand Coordinates
         for n,i in enumerate(now):
             x,y=self.canvas.coords(self.hands[n])[0:2]
@@ -54,6 +72,13 @@ class Clock(Tkinter.Tk):
             cr.append(self.length*math.cos(math.radians(i*6)-math.radians(90))+self.x)
             cr.append(self.length*math.sin(math.radians(i*6)-math.radians(90))+self.y)
             self.canvas.coords(self.hands[n], tuple(cr))
+        
+        # for n,i in enumerate(clock_home):
+        #     x,y=self.canvas.coords(self.hand[n])[0:2]
+        #     cr=[x,y]
+        #     cr.append(self.length*math.cos(math.radians(i*6)-math.radians(90))+self.x)
+        #     cr.append(self.length*math.sin(math.radians(i*6)-math.radians(90))+self.y)
+        #     self.canvas.coords(self.hand[n], tuple(cr))
         return
 
 # Main Function Trigger
