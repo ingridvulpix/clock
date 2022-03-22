785285eea8d1ed53c94ecdfd43563ab915f47f77
ingridvulpix <ingridalmeidafs@gmail.com>
2022-03-22 12:10:14 -0300

    24h clock

--- watch.py
+++ watch.py
@@ -5,7 +5,7 @@ import math
 import pytz
 import time
 
-
+#Measure for clock image and font size for messages
 WIDTH           = 300
 HEIGHT          = 300
 FONT_SIZE       = 7
@@ -15,20 +15,31 @@ IMG_SIZE            = (WIDTH,HEIGHT)
 ARROW_MIN_SIZE      = IMG_SIZE[1] - IMG_SIZE[1]*0.6
 ARROW_HOUR_SIZE     = IMG_SIZE[1] - IMG_SIZE[1]*0.8
 
+#Home and Local Timezones
 home_town = 'America/Bahia'
-local_tz = 'America/Toronto'
+local_tz = 'Asia/Tokyo'
 
-image = Image.open("white_clock.jpg")
+#Image file and end_prog for continuing or stopping program
+image = Image.open("24h_clock.jpg")
 end_prog = False
 
-class Clock:
-    """
-    Clock class implementation
-    Draws a clock with hour & minute arrows
-    acording to self timezone
-    """
 
+# Clock class implementation
+# Draws a clock with home hour, local hour, minute, seconds arrows
+# acording to self timezone (region and work_region)
+#
+class Clock:
+    ## Constructor.
+    #
+    # @param region home timezone.
+    # @param work_region local_tz.
+    # @param pos clock position.
+    # @param x x-axis coordinate
+    # @param y y-axis coordinate
+    #
     def __init__(self,region=home_town,work_region = local_tz,pos=[0,0],x=0,y=0):
+        """Constructor"""
+        
         self.grid = []
         self.region = region
         self.work_region = work_region
@@ -39,42 +50,57 @@ class Clock:
         self.x = x
         self.y = y
         self.pos = list(pos)
-
-    def add_clock(self,region,local_region,indx):
+    
+    # Adds a clock in the center of the canvas
+    #
+    def add_clock(self):
         pos = [0,0]
-        pos[0] = self.x+IMG_SIZE[0]*indx
+        pos[0] = self.x
         pos[1] = self.y
-        clk = Clock(region,local_region,pos)
+        clk = Clock(self.region,self.work_region,pos)
         self.grid.append(clk)
 
+    # Draws a clock in canvas
+    #
     def draw_clocks(self,canvas):
         for clk in self.grid:
             clk.draw_me(canvas)
 
-    def update_clocks(self,canvas):
+    # Updates clock with time passage
+    #
+    def update_clock(self,canvas):
         for clk in self.grid:
             if not (clk.seconds()%60):
                 clk.draw_minutes(canvas)
                 clk.draw_hours(canvas)
             clk.draw_seconds(canvas)
 
+    # Calculate and return the seconds from time zone
+    #
     def seconds(self):
-        """Returns the minutes from the current time zone"""
+        """Returns the seconds from time zone"""
         return datetime.now(pytz.timezone(self.region)).second
 
+    # Calculate and return the minutes from time zone
+    #
     def minutes(self):
-        """Returns the minutes from the current time zone"""
+        """Returns the minutes from time zone"""
         return datetime.now(pytz.timezone(self.region)).minute
 
+    # Calculate and return the hour from home time zone
+    #
     def hours(self):
         """Returns the hours from home time zone"""
         return datetime.now(pytz.timezone(self.region)).hour
 
+    # Calculate and return the hour from local time zone
+    #
     def hour_local(self):
         """Returns the hours from the current time zone"""
         return datetime.now(pytz.timezone(self.work_region)).hour
 
-
+    # Draw the seconds hand in shape of arrow
+    #
     def draw_seconds(self,canvas):
         if canvas:
             canvas.delete(self.line_sec_id)
@@ -87,7 +113,8 @@ class Clock:
             self.line_sec_id=canvas.create_line(x0,y0,x1,y1)
             canvas.pack()
 
-
+    # Draw the minutes hand in shape of arrow
+    #
     def draw_minutes(self,canvas):
         """ draw the minutes arrows"""
         if canvas:
@@ -104,6 +131,8 @@ class Clock:
             self.line_min_id = canvas.create_line(x0,y0,x1,y1, arrow=tk.LAST,arrowshape=(20,10,2))
             canvas.pack()
 
+    # Draw the home hour hand in shape of arrow
+    #
     def draw_hours(self,canvas):
         """ draw the hours arrows"""
         if canvas:
@@ -113,7 +142,7 @@ class Clock:
             h = self.hours()
 
             #add minutes factor influance on the hour angle
-            angle = -math.pi/2 + 2*math.pi*(h%12)/12 + 2*math.pi*m/12/60
+            angle = -math.pi/2 + 2*math.pi*(h%24)/24 + 2*math.pi*m/24/60
             x0 = self.pos[0]+IMG_SIZE[0]//2
             y0 = self.pos[1]+IMG_SIZE[1]//2
             x1 = x0 +ARROW_HOUR_SIZE*math.cos(angle)
@@ -122,6 +151,8 @@ class Clock:
             self.line_hour_id = canvas.create_line(x0,y0,x1,y1, arrow=tk.LAST,arrowshape=(20,10,2))
             canvas.pack()
 
+    # Draw the (local) hour hand in shape of arrow
+    #
     def draw_localh(self,canvas):
         """ draw the hours arrows from local region"""
         if canvas:
@@ -131,7 +162,7 @@ class Clock:
             h_local = self.hour_local()
 
             #add minutes factor influance on the hour angle
-            angle = -math.pi/2 + 2*math.pi*(h_local%12)/12 + 2*math.pi*m/12/60
+            angle = -math.pi/2 + 2*math.pi*(h_local%24)/24 + 2*math.pi*m/24/60
             x0 = self.pos[0]+IMG_SIZE[0]//2
             y0 = self.pos[1]+IMG_SIZE[1]//2
             x1 = x0 +ARROW_HOUR_SIZE*math.cos(angle)
@@ -140,6 +171,8 @@ class Clock:
             self.line_local_hour_id = canvas.create_line(x0,y0,x1,y1, arrow=tk.LAST,arrowshape=(15,10,2), fill = 'red')
             canvas.pack()
 
+    # Returns a string with de time for the digital clock
+    #
     def digital_time_str(self):
         """ returns hours & minutesstring hh:mm """
 
@@ -154,6 +187,9 @@ class Clock:
 
         return h_str + ":" + m_str
 
+    # Draw the components (clock, sticks, timezone, digital clock)
+    # in canvas
+    #
     def draw_me(self,canvas):
         canvas.create_image(self.pos[0], self.pos[1], image=photo, anchor='nw')
 
@@ -168,27 +204,29 @@ class Clock:
         x0 = self.pos[0]+IMG_SIZE[0]//2
         y0 = self.pos[1]+IMG_SIZE[1]//2
 
-        canvas.create_text(x0,y0-30,fill="Black",font="Calibri " + str(FONT_SIZE+1)+" italic bold",text=self.region)
-        canvas.create_text(x0,y0-20,fill="Red",font="Calibri " + str(FONT_SIZE)+" italic bold",text=self.work_region)
+        canvas.create_text(x0,y0-20,fill="Black",font="Calibri " + str(FONT_SIZE+1)+" italic bold",text=self.region)
+        canvas.create_text(x0,y0-10,fill="Red",font="Calibri " + str(FONT_SIZE)+" italic bold",text=self.work_region)
 
         digtime = self.digital_time_str()
         canvas.create_text(x0,y0+20,fill="Black",font="Calibri 20 italic bold",text=digtime)
         canvas.pack()
 
-
+# Stops the program
+#
 def quit():
     global end_prog
     end_prog = True
     quit()
 
 
-
+# Main function
+#
 def main():
     global image,photo,end_prog
 
     try :
         clk_display = Clock()
-        clk_display.add_clock(home_town,local_tz,0)
+        clk_display.add_clock()
         top = tk.Tk()
         top.title("AD1- Ingrid de Almeida")
         canvas = tk.Canvas(top, bg="silver", height=HEIGHT, width=WIDTH)
@@ -201,7 +239,7 @@ def main():
         clk_display.draw_clocks(canvas)
         while not end_prog:
             top.update()
-            clk_display.update_clocks(canvas)
+            clk_display.update_clock(canvas)
             time.sleep(1)
         top.mainloop()
     finally:
